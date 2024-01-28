03b83d305e302a0012d38b050772af9a06b7b739
Kazkrin <155957825+Kazkrin@users.noreply.github.com>
2024-01-28 16:43:51 +0100

    Update j_jboss.sh

--- j_jboss.sh
+++ j_jboss.sh
@@ -5,27 +5,27 @@ alias=$1
 declare -A array_host
 declare -A array_user
 
-array_host[dcsprd]="dcsprdapp.cargowrite.dsv.com"
-array_host[dcsprdb]="dcsprdappb.cargowrite.dsv.com"
-array_host[dcsprdc]="dcsprdappc.cargowrite.dsv.com"
-array_host[dcsprdd]="dcsprdappd.cargowrite.dsv.com"
-array_host[dcsprde]="dcsprdappe.cargowrite.dsv.com"
-array_host[dcsprdf]="dcsprdappf.cargowrite.dsv.com"
+array_host[dcsprd]=""
+array_host[dcsprdb]=""
+array_host[dcsprdc]=""
+array_host[dcsprdd]=""
+array_host[dcsprde]=""
+array_host[dcsprdf]=""
 
-array_user[dcsprd]="dcsprd"
-array_user[dcsprdb]="dcsprdb"
-array_user[dcsprdc]="dcsprdc"
-array_user[dcsprdd]="dcsprdd"
-array_user[dcsprde]="dcsprde"
-array_user[dcsprdf]="dcsprdf1"
+array_user[dcsprd]=""
+array_user[dcsprdb]=""
+array_user[dcsprdc]=""
+array_user[dcsprdd]=""
+array_user[dcsprde]=""
+array_user[dcsprdf]=""
 
-command="cd /share/dcsprdb/scripts/ && ./Jboss_Monitor.sh"
+command=""
 host=${array_host[$1]}
 user=${array_user[$1]}
 
 
 check() {
-result=$(ssh -i /home/nagios/ssh_priv_key -l $user $host "cd /share/$user/scripts/ && ./Jboss_Monitor.sh")
+result=$(ssh -i /home/nagios/ssh_priv_key -l $user $host "cd /")
 status=$?
 
 if [ $status -eq 0 ]
